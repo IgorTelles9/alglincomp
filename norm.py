5b74704458656aac047ec8cf4e39a26096835ccf
Igor Telles Barbosa <igortb@MacBook-Pro-de-Igor.local>
2022-05-10 12:47:16 -0300

    fix(norm.py): Add doc string

--- norm.py
+++ norm.py
@@ -1,4 +1,8 @@
 def euclidian(v:list, tolm:int=1):
+  ''' 
+  Calcula a norma euclidiana do vetor @param v, 
+  realizando arredondamento para @param tolm casas decimais. 
+  '''
   norm_sqr = 0
   for element in v:
     norm_sqr += (element ** 2)
