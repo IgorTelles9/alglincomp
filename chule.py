39d241ebe5208ccd701075ef9217b49d51ba0032
eduardocanova23 <64231485+eduardocanova23@users.noreply.github.com>
2022-05-19 03:03:58 -0300

    conserto do chule

--- chule.py
+++ chule.py
@@ -1,44 +1,38 @@
-
 from math import prod, sqrt
 from pprint import pprint 
+from utils import getTranspose
 
-def cholesky(A, ordem, idet):
+def cholesky(A, ordem, idet=False):
     # Quantidade de linhas/colunas da matriz A
     
-
+    L =[]
     # Inicializar L com um bando de 0s
-    L = [[0.0] * ordem for i in range(ordem)] 
+    for i in range(ordem):
+        L.append([])
+        for k in range(ordem):
+            L[i].append(0.0)
 
     # Iteração planejada de modo a formar L[i][k] como uma inferior
     for i in range(ordem):
         for k in range(i+1):   
-
+            
             soma_tmp = sum(L[i][j] * L[k][j] for j in range(k))   
 
             if (i == k):
                 check = A[i][i] - soma_tmp
                 if check <= 0:
-                    return 'deu merda', 'deu merda'                                 
+                    return 'matriz nao ta certa', 'matriz nao ta certa'                                 
                 L[i][k] = sqrt(check)     
 
             else:    
                 denominador = L[k][k] * (A[i][k] - soma_tmp) 
                 if denominador == 0:
-                    return 'deu merda', 'deu merda'                          
+                    return 'matriz nao ta certa', 'matriz nao ta certa'                          
                 L[i][k] = (1.0 /denominador)
     if idet:
         det = (prod(L[j][j] for j in range(ordem)))**2
     else:
         det = 'Determinante não calculado'
-    return L, det
 
-A = [[6, 3, 4, 8], [3, 6, 5, 1], [4, 5, 10, 7], [8, 1, 7, 25]]
-L, det = cholesky(A, 1)
-
-print("A:")
-pprint(A)
-
-print("L:")
-pprint(L)
-
-pprint(det)
+    Lt = getTranspose(L)
+    return L, Lt, det
