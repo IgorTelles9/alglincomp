a6d87f5cc86eab766eb3e68042f286bc9705b455
eduardocanova23 <64231485+eduardocanova23@users.noreply.github.com>
2022-06-27 18:37:20 -0300

    fix to increment t and round

--- rkn.py
+++ rkn.py
@@ -9,9 +9,8 @@ def rkn(h,temp,a1,a2,a3,w1,w2,w3,m,c,k):
     t=0
     y=0
     yl=0
-
-    contador = 0    
-    while contador <= temp:
+    
+    while t <= temp:
         k1 = 0.5*h*func(t,y,yl,a1,a2,a3,w1,w2,w3,m,c,k)
         q = 0.5*h*(yl+0.5*k1)
         k2 = 0.5*h*func(t+(h/2),y+q,yl+k1,a1,a2,a3,w1,w2,w3,m,c,k)
@@ -21,9 +20,9 @@ def rkn(h,temp,a1,a2,a3,w1,w2,w3,m,c,k):
 
         y = y + h*(yl+(1/3)*(k1+k2+k3))
         yl = yl + (1/3)*(k1+2*k2+2*k3+k4)
-
-        contador += k
-
+        t += h
+        t = round(t,1)
+        
     return y, yl
 
 #print(rkn(0.1,10,1,2,1.5,0.05,1,2,1,0.1,2))
