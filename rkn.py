d0063e0aff97f4011529427db95c088fe5d6a569
eduardocanova23 <64231485+eduardocanova23@users.noreply.github.com>
2022-06-27 02:46:27 -0300

    create runge kutta nystrom method

---
+++ rkn.py
@@ -0,0 +1,29 @@
+import math
+
+def func(t,y,yl,a1,a2,a3,w1,w2,w3,m,c,k):
+    return ((-c*yl)-(k*y)+(a1*math.sin(w1*t))+(a2*math.sin(w2*t))+(a3*math.cos(w3*t)))/m
+
+
+def rkn(h,temp,a1,a2,a3,w1,w2,w3,m,c,k):
+
+    t=0
+    y=0
+    yl=0
+
+    contador = 0    
+    while contador <= temp:
+        k1 = 0.5*h*func(t,y,yl,a1,a2,a3,w1,w2,w3,m,c,k)
+        q = 0.5*h*(yl+0.5*k1)
+        k2 = 0.5*h*func(t+(h/2),y+q,yl+k1,a1,a2,a3,w1,w2,w3,m,c,k)
+        k3 = 0.5*h*func(t+(h/2),y+q,yl+k2,a1,a2,a3,w1,w2,w3,m,c,k)
+        l = h*(yl+k3)
+        k4 = 0.5*h*func(t+h,y+l,yl+2*k3,a1,a2,a3,w1,w2,w3,m,c,k)
+
+        y = y + h*(yl+(1/3)*(k1+k2+k3))
+        yl = yl + (1/3)*(k1+2*k2+2*k3+k4)
+
+        contador += k
+
+    return y, yl
+
+#print(rkn(0.1,10,1,2,1.5,0.05,1,2,1,0.1,2))
