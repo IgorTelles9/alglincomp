421abae83e0fe5f8e1af5e4e517c52e525d79710
eduardocanova23 <64231485+eduardocanova23@users.noreply.github.com>
2022-06-28 00:17:35 -0300

    add task 2 ann task 3

--- p2.py
+++ p2.py
@@ -2,6 +2,9 @@ from math import exp
 import p2_task1;
 import p2_task2;
 import derivative;
+import gausslegendre;
+import richardson;
+import rkn;
 
 # TASK 1 
 
@@ -26,16 +29,24 @@ for i in range(1,3,1):
     print(task1(i,[0.75,6.5],[1,0,0],0.0001))
     print(task1(i,[0.00,11.667],[1,0,0],0.0001))
 
-def task2(icod, params, tolm,a=0,b=0,delta_x=0,opcao_der=0):
+def task2(icod, params, tolm,a=0,b=0,delta_x=0,opcao_der=0, n=0, delta1 = 0, delta2 = 0):
     f = lambda vars,x: vars[0]*exp(vars[1]*x) + vars[2]*x**vars[3]
 
     if icod == 1:
         return p2_task2.bissection(a,b,tolm,f,params)
     elif icod == 2:
-        pass
+        return gausslegendre.gausslegendre(a, b, n, params)
     elif icod == 3: 
-        derivative.first_order(f,a,delta_x,opcao_der,True,params)
+        return derivative.first_order(f,a,delta_x,opcao_der,True,params)
     elif icod == 4:
-        pass
+        return richardson.richardson(params,delta1,delta2,a)
 
-# print(task2(3,[-4,0,2,2], 0.001,2,2,0.0001,0))
+
+print(task2(4,[1,-2,-1,3],0.01, a=1, delta1 = 0.0100001, delta2 = 0.01))
+# print(task2(3,[-4,0,2,2], 0.001,2,2,0.0001,0))eee
+
+
+def task3(h, temp, params):
+    return rkn.rkn(h, temp, params)
+
+print(task3(0.1, 0.9, [1,2,1.5,0.05,1,2,1,0.1,2]))
